009beefd5e0a9f0a770f6e369b16b5a7afd7a8f5
Amos <amos2580@hotmail.co.uk>
2022-10-31 19:51:30 +0000

    Refactor and allow admins to set bank balances

--- source code/BankFix/Plugin.cs
+++ source code/BankFix/Plugin.cs
@@ -1,141 +1,88 @@
-﻿using SharedLibraryCore;
-using SharedLibraryCore.Interfaces;
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using SharedLibraryCore.Configuration;
-using System.Xml;
-using System.Collections.Generic;
-using SharedLibraryCore.Helpers;
+﻿using System.Globalization;
 using System.Text.RegularExpressions;
-using static System.Net.Mime.MediaTypeNames;
 using Newtonsoft.Json;
-using System.Text;
-using System.Threading.Tasks.Dataflow;
-using System.Linq;
-using SharedLibraryCore.Database.Models;
+using SharedLibraryCore;
+using SharedLibraryCore.Interfaces;
 
-namespace AutomessageFeed
+namespace BankFix;
+
+public class Plugin : IPlugin
 {
-    public class Plugin : IPlugin
+    public string Name => "Bank Fix";
+    public float Version => (float)Utilities.GetVersionAsDouble();
+    public string Author => "Doktor SAS & fed";
+    public const string BankBalanceKey = "BankBalance";
+    public static BankManager BankManager { get; private set; } = null!;
+
+
+    public Plugin(IServiceProvider serviceProvider)
     {
-        public string Name => "Bank Fix";
+        BankManager = new BankManager(serviceProvider);
+    }
 
-        public float Version => (float)Utilities.GetVersionAsDouble();
+    public async Task OnEventAsync(GameEvent gameEvent, Server server)
+    {
+        if (server.Gametype != "zclassic") return;
 
-        private readonly IMetaService _metaService;
-
-        public string Author => "Doktor SAS & fed";
-
-        public class BankClient
+        switch (gameEvent.Type)
         {
-            public BankClient(long Guid)
-            {
-                this.Guid = Guid;
-            }
-            public int Money = 0;
-            public long Guid;
-        }
-
-        public Plugin(IConfigurationHandlerFactory configurationHandlerFactory, ITranslationLookup translationLookup, IMetaService metaService)
-        {
-            _metaService = metaService;
-        }
-
-        public async Task<int> GetBankMeta(EFClient C)
-        {
-            if (await _metaService.GetPersistentMeta("BankBalance", C) == null)
-            {
-                _ = _metaService.AddPersistentMeta("BankBalance", "0", C);
-                return 0;
-            }
-            return Convert.ToInt32((await _metaService.GetPersistentMeta("BankBalance", C)).Value);
-        }
-
-        public async Task SetBankMeta(EFClient C, int Money)
-        {
-            await _metaService.AddPersistentMeta("BankBalance", Money.ToString(), C);
-        }
-
-        public async Task SetBankDvars(Server S)
-        {
-            // Sets dvar with information about connected players' money
-            string bankDvar = "";
-            for (int i = 0; i < S.Clients.Count; i++)
-            {
-                if (S.Clients[i] == null)
+            case GameEvent.EventType.Join:
+            case GameEvent.EventType.MapChange:
+                await BankManager.SetBankDVars(server);
+                break;
+            case GameEvent.EventType.Disconnect:
+                await BankManager.SetBankDVars(server);
+                break;
+            case GameEvent.EventType.Unknown:
+                if (Regex.Match(gameEvent.Data, @"IW4MBANK;(\d+);(\d+)").Length > 0)
                 {
-                    continue;
+                    // Saves bank account values - MATCHLIST = GUID ; BANK BALANCE
+                    var matchList = gameEvent.Data.Split(';');
+                    var client = server.GetClientsAsList()
+                        .Find(client => client.NetworkId == Convert.ToInt64(matchList[1]));
+                    if (client is null) return;
+                    await BankManager.SetBankMeta(client, int.Parse(matchList[2]));
+                    await BankManager.SetBankDVars(server);
                 }
-                bankDvar += (i > 0 ? "-" : "") + $"{S.Clients[i].NetworkId},{await GetBankMeta(S.Clients[i])}";
-            }
-            _ = S.RconParser.SetDvarAsync(S.RemoteConnection, "bank_clients_information", bankDvar);
-        }
 
+                if (Regex.Match(gameEvent.Data, @"IW4MBANK_ALL;(.)+").Length > 0)
+                {
+                    // Game ends, saves bank account values
+                    var json = gameEvent.Data[13..];
+                    var bankClients = JsonConvert.DeserializeObject<List<BankClient>>(json);
+                    if (bankClients is null) return;
 
-        public async Task OnEvent(GameEvent E, Server S)
-        {
-            if (S.Gametype != "zclassic")
-            {
-                return;
-            }
+                    var bankClientsDict = bankClients
+                        .GroupBy(client => client.Guid)
+                        .ToDictionary(key => key.Key
+                            .ConvertGuidToLong(NumberStyles.Integer), value => value.First().Money);
 
-            switch (E.Type)
-            {
-                case (GameEvent.EventType.PreConnect):
-                case (GameEvent.EventType.Join):
-                case (GameEvent.EventType.MapChange):
-                    _ = SetBankDvars(S);
-                    break;
-                case (GameEvent.EventType.Disconnect):
-                    _ = SetBankDvars(S);
-                    break;
-                case (GameEvent.EventType.Unknown):
-                    if (Regex.Match(E.Data, @"IW4MBANK;(\d+);(\d+)").Length > 0)
+                    foreach (var client in gameEvent.Owner.Manager.GetActiveClients()
+                                 .Where(client => bankClientsDict.ContainsKey(client.NetworkId)))
                     {
-                        // Saves bank account values
-                        string[] matchList = E.Data.Split(';');
-                        EFClient C = S.GetClientsAsList().Find(c => c.NetworkId == Convert.ToInt64(matchList[1]));
-                        await SetBankMeta(C, Convert.ToInt32(matchList[2]));
-                        _ = SetBankDvars(S);
+                        await BankManager.SetBankMeta(client, bankClientsDict[client.NetworkId]);
                     }
 
-                    if (Regex.Match(E.Data, @"IW4MBANK_ALL;(.)+").Length > 0)
-                    {
-                        // Game ends, saves bank account values
-                        string json = E.Data.Substring(13);
-                        List<BankClient> bankClients = JsonConvert.DeserializeObject<List<BankClient>>(json);
+                    await BankManager.SetBankDVars(server);
+                }
 
-                        foreach (BankClient client in bankClients)
-                        {
-                            EFClient C = S.GetClientsAsList().Find(c => c.NetworkId == Convert.ToInt64(client.Guid));
-                            await SetBankMeta(C, client.Money);
-                        }
-                        _ = SetBankDvars(S);
-                    }
-                    break;
-            }
+                break;
         }
+    }
 
-        public Task OnEventAsync(GameEvent E, Server S)
-        {
-            _ = OnEvent(E, S);
-            return Task.CompletedTask;
-        }
+    public Task OnLoadAsync(IManager manager)
+    {
+        Console.WriteLine($"BankFix loaded ({Author})");
+        return Task.CompletedTask;
+    }
 
-        public Task OnLoadAsync(IManager manager)
-        {
-            Console.WriteLine($"BankFix loaded ({Author})");
-        }
+    public Task OnTickAsync(Server server)
+    {
+        return Task.CompletedTask;
+    }
 
-        public Task OnTickAsync(Server S)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task OnUnloadAsync()
-        {
-            return Task.CompletedTask;
-        }
+    public Task OnUnloadAsync()
+    {
+        return Task.CompletedTask;
     }
 }
